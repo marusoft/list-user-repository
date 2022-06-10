885c9a61bb104f4a37c9e83fa7f9fc9f89b202d6
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2022-06-10 17:45:09 +0100

    implement navbar style

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,15 +5,20 @@ const Navbar = ({ user }) => {
   return (
     <div className="navbar">
       <span className="logo">Repo list</span>
-      <ul className="list">
-        <li className="listItem">
-          <img className="authorimg" src={user?.user.image} alt="author"  />
-        </li>
-        <li className="listItem">{user?.user.name}</li>
-        <li className="listItem" onClick={() => signOut()}>
-         <button className="logout"> Logout</button>
-        </li>
-      </ul>
+
+      {user ? (
+        <ul className="list">
+          <li className="listItem">
+            <img className="authorimg" src={user?.user.image} alt="author" />
+          </li>
+          <li className="listItem">{user?.user.name}</li>
+          <li className="listItem" onClick={() => signOut()}>
+            <button className="logout"> Logout</button>
+          </li>
+        </ul>
+      ) : (
+        <button className="logout" onClick={() => signIn("github")}>Login</button>
+      )}
     </div>
   );
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
@@ -9,19 +9,7 @@ const Home = () => {
   // console.log("USER", user)
   return (
     <div>
-      <Navbar  user={user} />
-      {user && (
-        <>
-          <h3>{user?.user.name}</h3>
-          <img src={user.user.image} alt="marusoft" width={100} height={100} />
-          <button onClick={() => signOut()}>Logout</button>
-        </>
-      )}
-      {!user && (
-        <>
-          <button onClick={() => signIn("github")}>Login</button>
-        </>
-      )}
+      <Navbar user={user} />
     </div>
   );
 };
